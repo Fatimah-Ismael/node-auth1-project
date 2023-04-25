4e18eccbb44d70f9a1a75f13de4f0f4ee50d2387
Fatimah Ismael <fatimahismael@yahoo.com>
2023-04-25 16:26:42 -0700

    wip

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -9,14 +9,12 @@ const User = require('../users/users-model')
   }
 */
 function restricted(req, res, next) {
-  console.log('users')
-  next()
-  /*if(req.session.user){
+  if(req.session.user){
     next()
   } else {
     next({ status: 401, message: 'You shall not pass!' })
   }
-*/
+
 }
 
 /*
